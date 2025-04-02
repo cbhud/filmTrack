303fb351dafd9864b44087e038890fe99aa7db90
Amer Hot <amerhott17@gmail.com>
2025-04-02 21:31:08 +0200

    Domaci 2 i sve sa casa
    Domaci 2 i sve sa casa

--- src/main/java/me/cbhud/model/Movie.java
+++ src/main/java/me/cbhud/model/Movie.java
@@ -1,12 +1,11 @@
 package me.cbhud.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import jakarta.persistence.*;
+
 import java.util.List;
 
 @Data
-//@Entity
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "movie_seq")
@@ -15,11 +14,6 @@ public class Movie {
     private String genre;
     private String director;
     private Integer releaseYear;
-//    @OneToMany
-//    @JoinColumn(name = "review_id")
-//    @JsonIgnore
-//    private List<Review> reviews;
-//    @ManyToMany(mappedBy = "watchedMovies")
-//    @JsonIgnore
-//    private List<Profile> users;
+    private List<Review> reviews;
+    private List<Profile> users;
 }
